1d1d99551a162a27395cfc2e153b9d56643d1c8a
SA1MAANKHAN <nazma9832@gmail.com>
2021-03-10 00:31:53 +0530

    amazon-clone began

---
+++ 18-amazon/amazon-clone/src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/Wireless/PrimeDay/LGWseries/LP/D11154727_LG_TopBanner_LP_PC_1500x500._CB442369035_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ 18-amazon/amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYuq7C1EDcpZiZ7WogNzvqVXkuCUr4HEM",
+  authDomain: "e-clone-33ffe.firebaseapp.com",
+  projectId: "e-clone-33ffe",
+  storageBucket: "e-clone-33ffe.appspot.com",
+  messagingSenderId: "145212773421",
+  appId: "1:145212773421:web:52fcec66280a45c093d4a4",
+  measurementId: "G-EQTFG29PR1",
+};
